05924b9d8d6b508073245afc95467e92c0ab2f60
Vauchel Johanna <j.vauchel@lectra.com>
2023-05-30 17:55:28 +0200

    add css

--- generate-prez.sh
+++ generate-prez.sh
@@ -3,5 +3,10 @@
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor \
   asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
   -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
+  -a customcss=custom.css \
   -D public -o index.html \
   dompter-data-talk.adoc
+
+cp custom.css public/
+
+touch public/.nojekyll

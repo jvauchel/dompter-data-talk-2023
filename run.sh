a673bdf63d230532dd3e34d09892fd22880c4061
Vauchel Johanna <j.vauchel@lectra.com>
2023-05-30 17:38:25 +0200

    build and deploy try 1

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor \
   asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
   -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
+  -D public -o index.html \
   dompter-data-talk.adoc

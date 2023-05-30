ae6b77d605ad0efb80fb05592964c5e5557e59aa
Vauchel Johanna <j.vauchel@lectra.com>
2023-05-30 18:54:42 +0200

    set slide design

--- generate-prez.sh
+++ generate-prez.sh
@@ -3,7 +3,7 @@
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor \
   asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
   -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
-  -a customcss=custom.css \
+  -a customcss=custom.css -a revealjs_slideNumber=true \
   -D public -o index.html \
   dompter-data-talk.adoc
 

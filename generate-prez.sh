afd081329f6c41824ec4b77702c79c5009fe07a3
Johanna Vauchel <vauchel.johanna@gmail.com>
2023-07-21 10:28:47 +0200

    generate several prez

--- generate-prez.sh
+++ generate-prez.sh
@@ -1,18 +1,47 @@
 #!/usr/bin/env bash
 
-docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor:1.49.0 \
-  asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
-  -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
-  -a customcss=custom.css -a revealjs_slideNumber=true \
-  -D public -o index.html \
-  dompter-data-talk.adoc
+echo "
+= Dompter Data
 
-cp custom.css public/
+" > index.adoc
+
+CONFERENCES="tadx jug-summer-camp abstract"
+
+for conf in ${CONFERENCES}
+do
+  echo "generate ${conf} html"
+
+  CONFERENCE_NAME=${conf}
+      CONFERENCE_PNG_BASE64=$(cat images/logo-${CONFERENCE_NAME}.png | base64 -w0) \
+      QRCODE_PNG_BASE64=$(cat images/qrcode.png | base64 -w0) \
+      MY_NAME_PNG_BASE64=$(cat images/myName.png | base64 -w0) \
+        envsubst < custom.css > public/custom-${conf}.css
+
+  if [ ${conf} = 'abstract' ] ; then
+    SOURCE="abstract.adoc"
+  else
+    SOURCE="dompter-data-talk.adoc"
+  fi
+
+  docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor:1.49.0 \
+    asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
+    -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
+    -a customcss=custom-${conf}.css -a revealjs_slideNumber=true \
+    -D public -o index-${conf}.html \
+    ${SOURCE}
+
+    echo "* link:index-${conf}.html[${conf}^]" >> index.adoc
+
+done
+
 mkdir -p public/videos
-mkdir -p public/images
 cp -a videos public
-cp images/myName.png public/images/
-cp images/qrcode.jpeg public/images/
-cp images/tadx.png public/images/
+
 
 touch public/.nojekyll
+
+
+docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor:1.49.0 \
+  asciidoctor -D public -o index.html index.adoc
+
+rm index.adoc
